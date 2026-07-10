84ef0d988b708beb5ab547509e7c788e45ff0951
9nstvr <sellman1ks4@gmail.com>
2026-07-10 10:21:02 +0300

    обновил app для amvera

--- app.py
+++ app.py
@@ -110,5 +110,8 @@ def history():
     )
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
+
